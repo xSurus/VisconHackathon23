374db47e5c361f505f021b69f835f9a2cb46d799
Fabio <fabrentsch@gmail.com>
2022-10-15 02:05:05 +0200

    add topbar

--- source/components/SupplierComponents/TopBar.tsx
+++ source/components/SupplierComponents/TopBar.tsx
@@ -1,8 +1,49 @@
+import { ClassNames } from '@emotion/react';
+import {makeStyles } from '@mui/styles';
+import { Button, Grid } from '@mui/material'
 import * as React from 'react';
+import { grey } from '@mui/material/colors';
 
-const TopBar = () => {
+const useStyles = makeStyles({
+    container:{
+        backgroundColor: 'grey', 
+        padding: '0px'
+    },
+    button:{
+        color: 'black',
+        padding: '2em',
+        fontSize: '18px',
+        font: 'bebas neue'
+    }
+});
+
+const TopBar = (index: number) => {
+    const classes = useStyles();
     return (
-        <div>test</div>
+        <Grid container className={classes.container} justifyContent={'center'}>
+            <Grid item >
+                <Button  className={classes.button}>
+                    My offers
+                </Button>
+                
+            </Grid>
+
+            <Grid item >
+                <Button className={classes.button}>
+                My Vouchers
+                </Button>
+            </Grid>
+
+            <Grid item >
+                <Button className={classes.button}>
+                Profile
+                </Button>
+            </Grid>
+
+            <Grid item>
+
+            </Grid>
+        </Grid>
     )
 }
 
