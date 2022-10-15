a6ceff551efa778cd9ca2c7d24f38677cef36039
YNezZ <ylliineziri@gmail.com>
2022-10-15 02:44:51 +0200

    fixing amos shit

--- pages/Registration.tsx
+++ pages/Registration.tsx
@@ -5,17 +5,18 @@ import { useState } from "react";
 const Registration = () => {
 
     const [form, showForm] = useState(<RegisterFormSupplier/>);
+    const [isRegisterFormSupplier, setIsRegisterFormSupplier] = useState<boolean>(true);
 
     return (
         <div>
             <h2 
-            onClick={() => showForm(<RegisterFormSupplier/>)} 
-            style={{   cursor:'pointer', display: 'inline'}}>
+            onClick={() => {showForm(<RegisterFormSupplier/>); setIsRegisterFormSupplier(true)}}
+            style={{color: isRegisterFormSupplier ? 'red' : 'black', cursor:'pointer', display: 'inline'}}>
                 Supplier
             </h2> 
             <h2 
-            onClick={() => showForm(<RegisterFormSeeker/>)} 
-            style={{cursor:'pointer', display: 'inline'}}>
+            onClick={() => {showForm(<RegisterFormSeeker/>); setIsRegisterFormSupplier(false)}}
+            style={{color: !isRegisterFormSupplier ? 'red' : 'black', cursor:'pointer', display: 'inline'}}>
                 Seeker
             </h2>
             { form }
