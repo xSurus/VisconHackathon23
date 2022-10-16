8c0fd55fa0822c3df224ad980d3e0c38c10428f2
Axel Montini <axel.montini@gmail.com>
2022-10-16 06:33:23 +0200

    fix data

--- pages/seeker.tsx
+++ pages/seeker.tsx
@@ -19,28 +19,27 @@ const SeparatorBar = styled(Grid)`
 `;
 
 type Data = { userType: string; token: string; id: number };
-
-const Seeker = ({ data }: { data: Data }) => {
+const HeaderM = styled(Grid)`
+	background-color: #white;
+	height: 3em;
+	width: 100%;
+	margin-bottom: 1em;
+`;
+const HeaderText = styled(Grid)`
+	font-size: 1.5em;
+	font-weight: bold;
+	text-align: center;
+	fontsize: large;
+`;
+const AccountIcon = styled(IconButton)`
+	float: right;
+	margin-top: 0.5em;
+	margin-right: 0.5em;
+`;
+const Seeker = (data: Data) => {
 	const [page, showPage] = useState(<SeekerOrders />);
 	const [whichPage, setWhichPage] = useState<number>(1);
 
-	const HeaderM = styled(Grid)`
-		background-color: #white;
-		height: 3em;
-		width: 100%;
-		margin-bottom: 1em;
-	`;
-	const HeaderText = styled(Grid)`
-		font-size: 1.5em;
-		font-weight: bold;
-		text-align: center;
-		fontsize: large;
-	`;
-	const AccountIcon = styled(IconButton)`
-		float: right;
-		margin-top: 0.5em;
-		margin-right: 0.5em;
-	`;
 	return (
 		<Grid container>
 			<Grid
